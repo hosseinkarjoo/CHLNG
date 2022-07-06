0ba834654f53712eafbc96399d1e1a292e874306
root <root@c4df90f5fc2c.mylabserver.com>
2022-07-06 07:11:36 +0000

    write if for url parameter

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/helloworld')
+@app.route('/helloworld', methods=['GET'])
 def helloworld():
+  Name = request.args.get('name')
+  if Name is None:
     return 'Hello Stranger'
+  else:
+    return 'Hello '+Name
