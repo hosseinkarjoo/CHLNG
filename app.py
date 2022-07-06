292d6d2613c059c0b792b106833241a84e1d46cf
root <root@c4df90f5fc2c.mylabserver.com>
2022-07-06 09:23:15 +0000

    add /versionz - only git hash

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask, request
 import re
+import subprocess
+
 app = Flask(__name__)
 
 @app.route('/helloworld', methods=['GET'])
 def helloworld():
-  Name = request.args.get('name')
-  Name = re.sub("([a-z])([A-Z])","\g<1> \g<2>", Name)
-  if Name is None:
+#  Name = request.args.get('name')
+  if request.args.get('name') is None:
     return 'Hello Stranger'
   else:
+    Name = request.args.get('name')
+    Name = re.sub("([a-z])([A-Z])","\g<1> \g<2>", Name)
     return 'Hello '+Name
+
+@app.route('/versionz')
+def versionz():
+  git_hash = subprocess.run(['git', 'rev-parse', 'HEAD'], stdout=subprocess.PIPE)
+  return git_hash.stdout.decode('utf-8')
