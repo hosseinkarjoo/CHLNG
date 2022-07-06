d2c5a14020269078f56cba3eb76faa1227d5cd1e
root <root@c4df90f5fc2c.mylabserver.com>
2022-07-06 07:37:10 +0000

    add camel-case seprate by space

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request
-
+import re
 app = Flask(__name__)
 
 @app.route('/helloworld', methods=['GET'])
 def helloworld():
   Name = request.args.get('name')
+  Name = re.sub("([a-z])([A-Z])","\g<1> \g<2>", Name)
   if Name is None:
     return 'Hello Stranger'
   else:
