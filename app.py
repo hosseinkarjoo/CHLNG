4de5c2cb04d0e58e2ba49881fc1c10a08b33de8d
root <root@c4df90f5fc2c.mylabserver.com>
2022-07-06 09:30:40 +0000

    adding comments

--- app.py
+++ app.py
@@ -6,15 +6,18 @@ app = Flask(__name__)
 
 @app.route('/helloworld', methods=['GET'])
 def helloworld():
-#  Name = request.args.get('name')
+  #checking if there is any url parameters
   if request.args.get('name') is None:
     return 'Hello Stranger'
   else:
+    #get the parameter
     Name = request.args.get('name')
+    #Camel-Case get by space with regex
     Name = re.sub("([a-z])([A-Z])","\g<1> \g<2>", Name)
     return 'Hello '+Name
 
 @app.route('/versionz')
 def versionz():
+  #getting the latest git hash
   git_hash = subprocess.run(['git', 'rev-parse', 'HEAD'], stdout=subprocess.PIPE)
   return git_hash.stdout.decode('utf-8')
