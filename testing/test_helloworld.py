47208d08194dd670a3631f89b574727eec5c63fc
root <root@c4df90f5fc2c.mylabserver.com>
2022-07-07 08:00:29 +0000

    unit test - hello + parameter

--- testing/test_helloworld.py
+++ testing/test_helloworld.py
@@ -12,4 +12,9 @@ def test_helloworld(client):
     assert isinstance(resp.json, dict)
     assert resp.json.get('message', 'Hello Stranger')
 
+def test_helloworld_name(client):
+    resp = client.get('/helloworld?name=HosseinKarjoo')
+    assert resp.status_code == 200
+    assert isinstance(resp.json, dict)
+    assert resp.json.get('message', 'Hello Hossein Karjoo')
 
