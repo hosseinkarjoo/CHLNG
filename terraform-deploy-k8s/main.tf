1c5faf62370388b739d1c6a082759b827b0e0f02
root <root@c4df90f5fc2c.mylabserver.com>
2022-07-09 17:08:42 +0000

    terraform deployment ports to port

--- terraform-deploy-k8s/main.tf
+++ terraform-deploy-k8s/main.tf
@@ -31,7 +31,7 @@ resource "kubernetes_deployment" "flask" {
         container {
           image = "937945817386.dkr.ecr.us-east-1.amazonaws.com/flask:latest"
           name  = "flask"
-          ports {
+          port {
             containerPort = "8080"
           }
         }
