e3676f9ed912754eeeea05e61326e150f197c1cb
hosseinkarjoo <75358989+hosseinkarjoo@users.noreply.github.com>
2022-07-10 00:07:10 +0430

    Update main.tf

--- terraform-deploy-k8s/main.tf
+++ terraform-deploy-k8s/main.tf
@@ -29,7 +29,7 @@ resource "kubernetes_deployment" "flask" {
 
       spec {
         container {
-          image = "937945817386.dkr.ecr.us-east-1.amazonaws.com/flask:latest"
+          image = "247083573128.dkr.ecr.us-east-1.amazonaws.com/flask:latest"
           name  = "flask"
           port {
             container_port = "8080"
