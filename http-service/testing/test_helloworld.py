904c6c9e65f191a9edea960aa7ce9368e4b93f21
hosseinkarjoo <75358989+hosseinkarjoo@users.noreply.github.com>
2022-07-10 13:20:50 +0430

    Update test_helloworld.py

--- http-service/testing/test_helloworld.py
+++ http-service/testing/test_helloworld.py
@@ -19,8 +19,8 @@ def test_helloworld_name(client):
 
 def test_versionz(client):
     resp = client.get('/versionz')
-    git_hash = os.getenv('GITHASH')
-    project_name = os.getenv('GITREPO')  
+    git_hash = bytes(os.getenv('GITHASH'))
+    project_name = bytes(os.getenv('GITREPO'))
     assert resp.status_code == 200
 #    assert isinstance(resp.json, dict)
-    assert b'git_hash + project_name' in resp.data
+    assert git_hash + project_name in resp.data
