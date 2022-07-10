b92b09e4146af9b07dfc30389279bad65e013f74
hosseinkarjoo <75358989+hosseinkarjoo@users.noreply.github.com>
2022-07-10 18:07:00 +0430

    Update main.tf

--- terraform-deploy-k8s/main.tf
+++ terraform-deploy-k8s/main.tf
@@ -29,7 +29,7 @@ resource "kubernetes_deployment" "flask" {
 
       spec {
         container {
-          image = "335090867831.dkr.ecr.us-east-1.amazonaws.com/flask:latest"
+          image = "335090867831.dkr.ecr.us-east-1.amazonaws.com/flask:latest"    #this is the address of the ECR that should be replace with new one
           name  = "flask"
           port {
             container_port = "8080"
@@ -51,7 +51,7 @@ resource "kubernetes_service" "flask-svc" {
     session_affinity = "ClientIP"
     port {
       port        = 8080
-      node_port = 32000
+      node_port = 32000    # publishing the service with NodePort and an aws LB routes the traffic to all replicas
     }
 
     type = "NodePort"
